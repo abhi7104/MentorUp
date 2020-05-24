9c4969c24c45355b1b1329c82301d0db824a7fd3
ShubhamB <shubhambhardwaj142@gmail.com>
2020-05-24 23:03:59 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://sam142:sam142@cluster0-2cwme.mongodb.net/MentorUp?retryWrites=true&w=majority'
+    MongoURI: 'mongodb+srv://username:<password>@cluster0-2cwme.mongodb.net/MentorUp?retryWrites=true&w=majority'
 }
